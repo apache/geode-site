c1e1f5ba34f0a4e0af949688890f67e7e7b3f8f7
Dave Barnes <dbarnes@apache.org>
2020-11-19 15:08:09 -0800

    Update geode-native manuals and API docs for Geode 1.13.1 release

---
+++ releases/latest/cppdocs/a00095.js
@@ -0,0 +1,4 @@
+var a00095 =
+[
+    [ "CqState", "a00095.html#aa532d841e6ee7cfa7f71dc37c551c71f", null ]
+];
---
+++ releases/latest/dotnetdocs/a01094.js
@@ -0,0 +1,12 @@
+var a01094 =
+[
+    [ "CreateObject", "a01094.html#af4fa044a16e0f27e438125d03e30d96d", null ],
+    [ "FromData", "a01094.html#af75d72e375954334c9f11ad1e7efe65b", null ],
+    [ "GetFieldName", "a01094.html#a4aea2cef72a21e8bcf6931e985398fa3", null ],
+    [ "GetFieldType", "a01094.html#a4096028bd472d47d3bdbcbcafde94ca8", null ],
+    [ "IsFieldIncluded", "a01094.html#a0f8e1cb1e96ea53e9efe42dbb0fcffee", null ],
+    [ "IsIdentityField", "a01094.html#a77edc9c5289c56e81150367a55656366", null ],
+    [ "ReadTransform", "a01094.html#ae02f0f5e7301525df6dcba830ea7667e", null ],
+    [ "ToData", "a01094.html#a0a7fee9ac7d5b32600ea1729e2a6b50a", null ],
+    [ "WriteTransform", "a01094.html#a514fe5de7c42032425d365f3f6dfdb0e", null ]
+];
